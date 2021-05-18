d34b909bcfb7f94bee2727ab56108ece58434511
Sergey-hub02 <s.park190802@gmail.com>
2021-05-18 11:27:25 +0300

    Реализовал функцию нахождения последнего вхождения подстроки

--- src/includes/tasks.ts
+++ src/includes/tasks.ts
@@ -14,3 +14,22 @@ export const findFirst = (str: string, substr: string): number => {
 
   return -1;
 }
+
+
+/**
+ * Возвращает индекс последнего вхождения подстроки. Если такой подстроки нет, то возвращается отрицательное значение
+ * @param str          строка, в которой происходит поиск
+ * @param substr       подстрока
+ */
+export const findLast = (str: string, substr: string): number => {
+  for (let i: number = str.length - 1; i >= substr.length - 1; --i) {
+    const subIndex: number = i - substr.length + 1;
+    const sub: string = str.substr(subIndex, substr.length);
+
+    if (sub === substr) {
+      return subIndex;
+    }
+  }
+
+  return -1;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 
-import { findFirst } from "./includes/tasks";
+import { findFirst, findLast } from "./includes/tasks";
 import { _INPUT_FILE_NAME, _PATH_TO_INPUT_FILE_ } from "./constants";
 
 // Подобие стандартного потока ввода
@@ -12,12 +12,15 @@ const _STDIN_: string = fs.readFileSync(_PATH_TO_INPUT_FILE_ + _INPUT_FILE_NAME,
  */
 const main = async (): Promise<void> => {
   const [sentence, substring] = _STDIN_.split("\n");
+
   const first: number = findFirst(sentence, substring);
+  const last: number = findLast(sentence, substring);
 
   console.log(`Предложение: ${sentence}`);
   console.log(`Нужно найти: ${substring}`);
 
-  console.log(`Индекс: ${first}`);
+  console.log(`Индекс первого вхождения: ${first}`);
+  console.log(`Индекс последнего вхождения: ${last}`);
 }
 
 
